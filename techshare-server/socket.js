bab09c20a05b14ddba1f68cb7ef7182bc8255214
techsharebhtdev-04 <techshare.bht.dev@gmail.com>
2026-06-27 02:30:05 +0700

    Update socket.js

--- techshare-server/socket.js
+++ techshare-server/socket.js
@@ -5,7 +5,7 @@ let io;
 function initSocket(server) {
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:3000", "https://techshare-beta.vercel.app",
+      origin: "http://localhost:3000",
       credentials: true,
     },
   });
